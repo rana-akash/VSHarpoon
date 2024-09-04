3ac007c21a050dfd34a37fb7606dad62e0ed0486
aksah <akashrana3333@gmail.com>
2024-09-04 09:42:06 +0530

    half page scroll up and down

--- VSHarpoon/Commands/AppendTab.cs
+++ VSHarpoon/Commands/AppendTab.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Microsoft.VisualStudio.Text.Formatting;
+using System.Reflection;
 using System.Windows.Markup;
 
 namespace Test1
@@ -21,6 +22,65 @@ namespace Test1
                         break;
                     }
                 }
+            }
+        }
+    }
+
+    [Command(PackageIds.ScrollHalfPageDown)]
+    public sealed class ScrollHalfPageDown : BaseCommand<ScrollHalfPageDown>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var doc = await VS.Documents.GetActiveDocumentViewAsync();
+            var top = doc.TextView.ViewportTop;
+            var bottom = doc.TextView.ViewportBottom;
+            try
+            {
+                doc.TextView.ViewScroller.ScrollViewportVerticallyByPixels((top - bottom) / 2);
+                var lines = doc.TextView.TextViewLines.WpfTextViewLines;
+
+                var firstLineIndex = doc.TextView.TextViewLines.GetIndexOfTextLine(doc.TextView.TextViewLines.FirstVisibleLine);
+                var lastLineIndex = doc.TextView.TextViewLines.GetIndexOfTextLine(doc.TextView.TextViewLines.LastVisibleLine);
+
+                int midIndex = (int)((lastLineIndex - firstLineIndex)/ 2);
+
+                if (midIndex <= lines.Count-1)
+                {
+                    doc.TextView.Caret.MoveTo(lines[midIndex]);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+
+    [Command(PackageIds.ScrollHalfPageUp)]
+    public sealed class ScrollHalfPageUp : BaseCommand<ScrollHalfPageUp>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var doc = await VS.Documents.GetActiveDocumentViewAsync();
+            var top = doc.TextView.ViewportTop;
+            var bottom = doc.TextView.ViewportBottom;
+            try
+            {
+                doc.TextView.ViewScroller.ScrollViewportVerticallyByPixels((top - bottom) / -2);
+                var lines = doc.TextView.TextViewLines.WpfTextViewLines;
+
+                var firstLineIndex = doc.TextView.TextViewLines.GetIndexOfTextLine(doc.TextView.TextViewLines.FirstVisibleLine);
+                var lastLineIndex = doc.TextView.TextViewLines.GetIndexOfTextLine(doc.TextView.TextViewLines.LastVisibleLine);
+
+                int midIndex = (int)((lastLineIndex - firstLineIndex) / 2);
+
+                if (midIndex <= lines.Count - 1)
+                {
+                    doc.TextView.Caret.MoveTo(lines[midIndex]);
+                }
+            }
+            catch (Exception ex)
+            {
             }
         }
     }
--- VSHarpoon/VSCommandTable.cs
+++ VSHarpoon/VSCommandTable.cs
@@ -54,5 +54,7 @@ namespace Test1
         public const int SaveCurrentSession = 0x0132;
         public const int ScoochIndexes = 0x0133;
         public const int SaveAllSessions = 0x0134;
+        public const int ScrollHalfPageDown = 0x0135;
+        public const int ScrollHalfPageUp = 0x0136;
     }
 }
